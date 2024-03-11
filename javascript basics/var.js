ccb8c9e9894ed56ae7eb6025dbc4f4391fd3e2ce
Shrishailya Abhay Keskar <130174278+hecker03@users.noreply.github.com>
2024-03-11 16:38:05 +0000

    Variables

---
+++ javascript basics/var.js
@@ -0,0 +1,11 @@
+const accountID = 12245
+let accountemail = "shrishailya@gmail.com"
+var password = "23350"
+acountcity = "Pune"
+// accountID = 23
+
+accountemail ="sshri@gmail.com"
+password = "123"
+acountcity = "Kolhapur"
+console.log(accountID);
+console.table([accountID, acountcity, password, accountemail])
