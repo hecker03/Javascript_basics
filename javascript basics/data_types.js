aeaac3b4781891c3166b6913b47b6e14d5014f87
Shrishailya Abhay Keskar <130174278+hecker03@users.noreply.github.com>
2024-03-17 10:33:42 +0000

    Data types

---
+++ javascript basics/data_types.js
@@ -0,0 +1,22 @@
+// let name = "string"
+// let t = 1 
+// b = Boolean(t)
+// let age = 19
+// let staand;
+// console.log(typeof undefined);
+// console.log(typeof null);
+// console.log(b);
+
+//  conversion 
+/*
+let score = 33
+console.log(typeof score);
+let value = 3
+let negValue = -value
+console.log(negValue);
+console.log(+true)
+*/
+let gameCounter = 100
+gameCounter++;
+// console.log(gameCounter);
+console.log(true);
