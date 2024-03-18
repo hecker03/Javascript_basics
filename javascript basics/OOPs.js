60960b2d1e5daed89da1b6efa6a0e5081de55780
Shrishailya Abhay Keskar <130174278+hecker03@users.noreply.github.com>
2024-03-18 04:01:15 +0000

    Object

--- javascript basics/OOPs.js
+++ javascript basics/OOPs.js
@@ -1,7 +1,9 @@
 // Singleton
 Object.create // constuctor
 // object literals - 
-
+const newObject = new Object
+// or 
+const n = {}
 // symbols
 let mysym = Symbol("key1")
 const jsuser = {
@@ -10,7 +12,21 @@ const jsuser = {
     age : 18,
     email : "shri@gmail.com"
 }
-console.log(jsuser.name)
-console.log(jsuser["email"])
-console.log(jsuser[mysym])
-console.log(typeof jsuser[mysym])
+// console.log(jsuser.name)
+// console.log(jsuser["email"])
+// console.log(jsuser[mysym])
+// console.log(typeof jsuser[mysym])
+jsuser.age = 19 
+// Object.freeze(jsuser)
+jsuser.age = 18 
+// console.log(jsuser)
+
+jsuser.hi = function(){
+    console.log("Hello jsusers ")
+}
+jsuser.hi2 = function(){
+    console.log(`Hello , ${this.name}`);
+}
+
+console.log(jsuser.hi())
+console.log(jsuser.hi2())
