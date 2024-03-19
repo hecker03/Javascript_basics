7fb7c59c24b014f8bd3fee330d18d9d89f1a4739
Shrishailya Abhay Keskar <130174278+hecker03@users.noreply.github.com>
2024-03-19 12:04:05 +0000

    func

--- javascript basics/function.js
+++ javascript basics/function.js
@@ -12,4 +12,37 @@ function login(user = "Sam"){
     }
     return `${user} just logged in`
 }
-console.log(login())
+// console.log(login())
+
+function handlemultiple(num1, num2,...num){
+    console.log(num1, num2);
+    return num
+}
+// console.log(handlemultiple(100,841,4861,93));
+
+//How to handle objects in functions 
+let obj = {
+    username: "Sakshi",
+    price : 0
+}
+
+function handleobject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+// handleobject(obj)
+handleobject({
+    username : "Sam",
+    price : "very much valuable"
+})
+
+
+// ways of declaring functions
+hi() // this will not give you error
+function hi(){
+    return 1
+}
+
+// bye() // this will create an error
+let bye = function(){
+    return 2 
+}
