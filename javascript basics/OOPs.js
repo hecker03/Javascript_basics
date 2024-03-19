42dd620bacb6fc171745aa7c568d19f4410b7f79
Shrishailya Abhay Keskar <130174278+hecker03@users.noreply.github.com>
2024-03-19 11:14:07 +0000

    Object

--- javascript basics/OOPs.js
+++ javascript basics/OOPs.js
@@ -28,5 +28,34 @@ jsuser.hi2 = function(){
     console.log(`Hello , ${this.name}`);
 }
 
-console.log(jsuser.hi())
-console.log(jsuser.hi2())
+// console.log(jsuser.hi())
+// console.log(jsuser.hi2())
+
+let regular = {
+    name : {
+        firstname : "Shrishailya",
+        lastname : "Keskar",
+        username : "Sak"
+    },
+    email : "shree@goggle.com" 
+}
+// console.log(regular.name?.username);
+
+const obj1 = {1: "A", 2: "B"}
+const obj2 = {3: "C", 2: "D"}
+let obj3 = {...obj1, ...obj2}
+
+// another way - assign , by the module object 
+
+let obj4 = Object.assign({},obj1, obj2) // returning a empty object to store all the values 
+
+// console.log(obj4);
+
+// destructuring
+let Obj_ = {
+    employeeid : 101,
+    employeename : "Sam",
+    Salary : 200000
+}
+const {employeename:Name} = Obj_
+console.log(Name);
