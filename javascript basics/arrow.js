141a2689efadb1c221292551e19e3125cb325c58
Shrishailya Abhay Keskar <130174278+hecker03@users.noreply.github.com>
2024-03-24 07:39:31 +0000

    this and arrow function

---
+++ javascript basics/arrow.js
@@ -0,0 +1,25 @@
+let user = {
+    username : "SAK01",
+    price : 2000,
+
+    welcomemessage: function(){
+        console.log(`Hello ${this.username} !,Welcome to the website`)
+    }
+}
+
+// user.welcomemessage()
+// user.username = "Sam"
+// user.welcomemessage() // not hardcoded
+
+// console.log(this);
+
+// function code(){
+//     console.log(this);
+// }
+
+// code()
+
+// arrow functions
+let chai = (num1, num2) => (num1 +num2) 
+
+console.log(chai(3,4))
